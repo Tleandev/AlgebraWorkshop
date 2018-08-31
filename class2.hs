5e4350ff622c0778318beb28b224365b694e17e7
leandro silvestre di vito <ldivito@ws20.labo4>
2018-08-31 14:49:55 -0300

    added maximo functions

--- class2.hs
+++ class2.hs
@@ -9,3 +9,9 @@ signo n | n > 0 = 1
 absoluto n | n == 0 = 0
 		   | n > 0 = n
 		   | n < 0 = n * (-1)
+
+maximo a b | a < b = b
+		   | a > b = a
+		   | otherwise = a
+
+maximo3 a b c = maximo(maximo a b) c
