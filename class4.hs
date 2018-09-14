3a68a0fadcf6d335a7b26ec03f05615537c92a1e
leandro silvestre di vito <ldivito@ws20.labo4>
2018-09-14 15:06:54 -0300

    Added new functions

--- class4.hs
+++ class4.hs
@@ -13,3 +13,16 @@ sucesion1 :: Integer -> Integer
 sucesion1 n 
 	| n == 1 = 2
 	| otherwise = 2 * (n-1) * sucesion1(n-1) + 2 ^ n * factorial(n-1)
+
+sucesion2 :: Integer -> Integer
+sucesion2 n
+	| n == 1 = 1
+	| n == 2 = 2
+	| otherwise = (n-2) * sucesion2(n-1) + 2 * (n - 1) * sucesion2(n-2)
+
+sucesion3 :: Integer -> Integer
+sucesion3 n
+	| n == 1 = -1
+	| n == 2 = 6
+	| mod n 2 /= 0 = -1 * sucesion3(n-1) - 3
+	| mod n 2 == 0 = sucesion3(n-3) + 2 * sucesion3(n-2) + 9
